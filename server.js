d2eaee9987e610c5d7e535f95960a350bc59a0b4
Nitesh saini <itsniteshsaini6376@gmail.com>
2024-10-30 01:12:50 +0530

    Updated

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');  
 const bodyParser = require('body-parser');
-const ejsMate = require("ejs-mate");
 const mainrouter = require("./routes/mainroute")
 const app = express();
 

2de1474bdcb6ef11e1aebf3ad6786b4d3c73ae7d
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-04-12 01:03:26 +0530

    Solve CORS

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://portfolio-frontend-peach-one.vercel.app/',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   };

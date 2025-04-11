84e07f1b0c2236c7ffed27c7592cee82ebb2968b
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-04-12 02:24:10 +0530

    Vercel.json

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const path = require('path');  
 const bodyParser = require('body-parser');
-const mainrouter = require("./routes/mainroute")
+const mainrouter = require("./routes/mainroute");
 const app = express();
-const cors = require('cors');
 
+const allowCors = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-frontend-peach-one.vercel.app'); // Replace with your frontend URL
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    );
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+    next(); 
+};
 
-const corsOptions = {
-    origin: 'https://portfolio-frontend-peach-one.vercel.app/',
-    methods: 'GET,POST,PUT,DELETE', 
-    credentials: true, 
-    allowedHeaders: 'Content-Type,Authorization'
-  };
-  
+app.use(allowCors);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
-
-
-let port = 8080 ;
 
 app.use("/", mainrouter);
 
+let port = 8080;
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
